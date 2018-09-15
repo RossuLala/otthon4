88b24989b49f61b28c5e438debf0db005f5704b2
RossuLala <lala@intralog.hu>
2018-09-15 21:54:49 +0200

    Komponensek létrehozása (csontváz szétdarabolása)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopHeaderComponent,
+    LeftSidebarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
