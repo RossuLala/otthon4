9403eec8a2ba3ec64ab85c5937f059e3b4dcf1c9
RossuLala <lala@intralog.hu>
2018-09-16 07:42:06 +0200

    KOMPONENS RÉSZEI - megjegyzések

--- src/app/top-header/top-header.component.ts
+++ src/app/top-header/top-header.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import {environment} from '../../environments/environment';
+import {environment} from '../../environments/environment'; //IMPORT - használni szeretném a {} ban megatott komponest amit itt találok FROM
 
 
-@Component({
-  selector: 'app-top-header',
-  templateUrl: './top-header.component.html',
-  styleUrls: ['./top-header.component.css']
+@Component({ //komponens leírása
+  selector: 'app-top-header', //Így tudom HTML-ben meghívni ezt a componest:{{SELECTOR}}
+  templateUrl: './top-header.component.html', //itt találom a komponens HTLM fálját
+  styleUrls: ['./top-header.component.css'] //A stílusa itt található
 })
-export class TopHeaderComponent implements OnInit {
+export class TopHeaderComponent implements OnInit { // EXPORTÁLOK egy CLASS-t és egy IMPLEMENT-et
   appTitle:string = "";
 
-  constructor() { 
+  constructor() { //CONSTRUKTOR-ban adom meg, hogy mit mi csináljon a komponens
     //console.log(environment.appTitle)
     this.appTitle = environment.appTitle;
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+export const environment = { //EXPORT- eléthetővé teszek másik modulok számára a CONST változókat "envi.." néven
   production: false,
   appTitle:"OTTHON4"
 };
