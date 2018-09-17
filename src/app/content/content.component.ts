8519a5cbe5d6c7d75a6988d742deacc5b54cd88d
RossuLala <lala@intralog.hu>
2018-09-17 18:14:08 +0200

    Tömb elemeinek betöltése táblázatba

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -6,8 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+  user:{   // TÍPUS DEKLARÁCIÓ - létrehozok egy usert és TypeScrip-ben megadhatom az adatok típusát
+    id:number, 
+    lastName?:string,
+    firstName?:string,
+    email?:string,
+    phone?:string,
+    relatives?:string  //?- nem kötelező kitölteni
+   }
 
-  constructor() { }
+   users = [];
+
+  constructor() {
+    let user = {
+      id: 1,
+      lastName:"Kiss",
+      firstName:"Éva",
+      email:"lala@intralog.hu",
+      phone: "30-99-67-670",
+      relatives: "WINETOU"
+    }
+
+    let user2 = {
+      id: 1,
+      lastName:"Kiss",
+      firstName:"Éva",
+      email:"lala@intralog.hu",
+      phone: "30-99-67-670",
+      relatives: "WINETOU"
+    }
+
+    this.users.push (user);
+    this.users.push (user2);
+   }
 
   ngOnInit() {
   }
