445a2dc2e67255d9d189a38338946d8d41e742a8
RossuLala <lala@intralog.hu>
2018-09-17 19:24:27 +0200

    proba adatok módosítása

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -8,20 +8,17 @@ import { User } from '../model/User';
 })
 export class ContentComponent implements OnInit {
 
+  users = [];
 
-   users = [];
-
-  constructor() {
-
-  }
+  constructor() {}
 
   ngOnInit() {
-    let user1 = new User (1,'aaa', 'bbb','ccc','ddd')
-    let user2 = new User (1,'aaa', 'bbb','ccc','ddd')
-    let user3 = new User (1,'aaa', 'bbb','ccc','ddd')
+
+    let user1 = new User (1,'aaa1', 'bbb1','ccc1','ddd1')
+    let user2 = new User (2,'aaa2', 'bbb2','ccc2','ddd2')
+    let user3 = new User (1,'Rossu', 'László','lala@intralog.hu','30-99-67-670')
     this.users.push (user1);
     this.users.push (user2);
     this.users.push (user3);
   }
-
 }
