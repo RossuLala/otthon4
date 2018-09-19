2c495bd94954842c45d9e1ac5e44dc6ad8002d68
RossuLala <lala@intralog.hu>
2018-09-19 13:07:15 +0200

    MENÜ pontok aktívvá válnak a kattintásra

--- src/app/left-sidebar/left-sidebar.component.ts
+++ src/app/left-sidebar/left-sidebar.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left-sidebar.component.css']
 })
 export class LeftSidebarComponent implements OnInit {
+  private currentLink:string = "/";
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onMenuClick($event:Event){
+    $event.preventDefault();  //megakadályozza, hogy frissüljön az oldal
+    let element = $event.target as HTMLLinkElement;
+    this.currentLink = element.href.replace(location.origin,""); //CONSOLE-> location. beírásakor lászanak a választási lehetőségel
+    console.log(this.currentLink);
   }
 
 }
