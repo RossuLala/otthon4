f6d97d48a4943d253010dba3012fdfa39a8604ff
RossuLala <lala@intralog.hu>
2018-10-16 16:11:36 +0200

    Menükezelés rooter használatával

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,62 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
 import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
 import { ContentComponent } from './content/content.component';
 import { UserManagerComponent } from './content/user-manager/user-manager.component';
-import { UserService } from './user.service';
 import { UserEditorComponent } from './content/user-manager/user-editor/user-editor.component';
-import { UrlService } from './url.service';
 import { NewUserComponent } from './content/user-manager/new-user/new-user.component';
 import { UserTableRowComponent } from './content/user-table-row/user-table-row.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { SupplyComponent } from './supply/supply.component';
+
+
+import { UserService } from './user.service';
+import { UrlService } from './url.service';
 import { ConfigService } from './config.service';
-import { HttpModule } from '@angular/http';
 import { HttpService } from './http.service';
 
+
+const routerSetting: Routes = [
+    { path: '', component: ContentComponent },
+    { path: 'user-manager', component: UserManagerComponent },
+    { path: 'rooms', component: RoomsComponent },
+    { path: 'supply', component: SupplyComponent },
+    { path: '**', component: PageNotFoundComponent },//hiba oldal 404hiba.
+]
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    TopHeaderComponent,
-    LeftSidebarComponent,
-    ContentComponent,
-    UserManagerComponent,
-    UserEditorComponent,
-    NewUserComponent,
-    UserTableRowComponent,
-  ],
-  imports: [  //ide kell rakni a más aáltal készített modulokat
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [ //saját modulokat ide tesszük
-    UserService, 
-    UrlService,
-    ConfigService,
-    HttpService
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TopHeaderComponent,
+        LeftSidebarComponent,
+        ContentComponent,
+        UserManagerComponent,
+        UserEditorComponent,
+        NewUserComponent,
+        UserTableRowComponent,
+        PageNotFoundComponent,
+        RoomsComponent,
+        SupplyComponent,
+    ],
+    imports: [  //ide kell rakni a más által készített modulokat
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(routerSetting)
+    ],
+    providers: [ //saját modulokat ide tesszük
+        UserService,
+        UrlService,
+        ConfigService,
+        HttpService
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
