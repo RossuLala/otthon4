074e3d50664e617728cdffcc5c6aa0621366cd37
RossuLala <lala@intralog.hu>
2018-10-17 09:47:43 +0200

    ÖRÖKLŐDÉS - Szobák létrehozása -CRUD

--- src/app/http-room.service.ts
+++ src/app/http-room.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpService } from './http.service';
 import { Http, Response } from '@angular/http';
 import { ConfigService } from './config.service';
+import { Room } from './model/Room';
 
 @Injectable()
 export class HttpRoomService extends HttpService {// Örökítjük a HttpService-ből
@@ -23,5 +24,17 @@ export class HttpRoomService extends HttpService {// Örökítjük a HttpService
      
             }
         );
+    }
+
+    createRoom(room:Room){
+        return this.create(`${this.baseUrl}/room`, room);
+    }
+
+    updateRoom(room:Room){
+        return this.update(`${this.baseUrl}/room/${room.id}`, room);
+    }
+
+    deleteRoom(room:Room){
+        return this.delete(`${this.baseUrl}/room/${room.id}`);
     }
 }
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,20 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import {Room} from '../model/Room';
+import { Component, OnInit, Output } from '@angular/core';
+import { Room } from '../model/Room';
 import { HttpRoomService } from '../http-room.service';
 
 @Component({
-  selector: 'app-rooms',
-  templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.css']
+    selector: 'app-rooms',
+    templateUrl: './rooms.component.html',
+    styleUrls: ['./rooms.component.css']
 })
 export class RoomsComponent implements OnInit {
-  rooms: Array<Room> = [];
-  constructor(
-    private service: HttpRoomService 
-  ) { }
+    rooms: Array<Room> = [];
+    @Output() keys: Array<{ key: string, label: string }> = [
+        { key: "id", label: "#" },
+        { key: "name", label: "név" },
+        { key: "size", label: "méret" },
+        { key: "slot", label: "férőhely" },
+        { key: "floor", label: "emelet" },
+        { key: "active", label: "aktív" }
+    ]
 
-  ngOnInit() {
-    this.service.getAll();
-  }
+    @Output() newRoom: Room = new Room(33, "kék szoba");
 
+    constructor(
+        private service: HttpRoomService
+    ) { }
+
+    ngOnInit() {
+        this.service.getAll();
+    }
+
+    createRoom() {
+        this.service.createRoom(this.newRoom)
+        .then(
+            (res:Response) => {
+                console.log(res)
+            }
+        )
+    }
 }
